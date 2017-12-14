02d6ceadcbabbb8844dc0c71f11c3d2d5e991ade
Edmarv Allen Abril <edmarvallenabril@gmail.com>
2017-12-14 09:18:30 +0000

    DATABASEURL env variable

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ var commentRoutes = require("./routes/comments"),
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASEURL, {useMongoClient: true}); //export DATABASEURL
-// mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
-// mongoose.connect("mongodb://edmarv:yelpadmin@ds135946.mlab.com:35946/yelpcamp_v12", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
